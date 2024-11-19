fb771f8731504d89b9c0ffa7a59077e5d2daa14d
RedLintu16 <redlintu16@gmail.com>
2024-11-19 15:06:44 +0000

    Initinal LineageOS device tree.

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 #
 
 # Get common SoC device configs
-$(call inherit-product, device/nubia/sm8650-common/pineapple.mk)
+#$(call inherit-product, device/nubia/sm8650-common/pineapple.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/nubia/nx721j/nx721j-vendor.mk)
@@ -14,6 +14,15 @@ $(call inherit-product, vendor/nubia/nx721j/nx721j-vendor.mk)
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Audio
+SOONG_CONFIG_NAMESPACES += android_hardware_audio
+SOONG_CONFIG_android_hardware_audio += run_64bit
+SOONG_CONFIG_android_hardware_audio_run_64bit := true
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1.vendor
+
 # Audio configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_pineapple/audio_effects.xml \
@@ -21,9 +30,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_pineapple/audio_policy_configuration.xml
 
 # Shipping API level
-BOARD_API_LEVEL := 34
-BOARD_SHIPPING_API_LEVEL := 34
-PRODUCT_SHIPPING_API_LEVEL := 34
+#BOARD_API_LEVEL := 34
+#BOARD_SHIPPING_API_LEVEL := 34
+#PRODUCT_SHIPPING_API_LEVEL := 34
 
 # Vibrator configs
 PRODUCT_COPY_FILES += \
@@ -36,3 +45,10 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Boot control
+PRODUCT_PACKAGES += \
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery \
+    libboot_control_qti \
+    libgptutils.pineapple
